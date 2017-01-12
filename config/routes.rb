471811d3d12a5fe4ded0b15f8ab82df6dbbe783c
mippet <pimluitjes@gmail.com>
2017-01-12 15:52:32 +0100

    create and show new dino pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "dinosaurs#index"
 
-  get "dinosaurs" => "dinosaurs#index"
+  get 'dinosaurs' => 'dinosaur#index'
+  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  get 'dinosaurs/:id' => 'dinosaurs#show', as: :dinosaur
+
+  post "dinosaurs" => "dinosaurs#create"
  end
